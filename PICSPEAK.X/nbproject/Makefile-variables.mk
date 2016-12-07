52812ada7dab2fdcf5d7c989a8c37ca9a3154592
luciodj <pilot@flyingpic24.com>
2016-12-07 11:31:44 +0100

    First port to PIC10F32x

---
+++ PICSPEAK.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=PICSPEAK.X.production.null
+CND_ARTIFACT_PATH_default=dist/default/production/PICSPEAK.X.production.null
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=picspeak.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/picspeak.x.tar
